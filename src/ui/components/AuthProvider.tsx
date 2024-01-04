e452b54ce5e4306017e10f6a08c1af07a402a335
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2024-01-04 16:29:13 +0300

    update the gitignore to not track build outputs

--- src/ui/components/AuthProvider.tsx
+++ src/ui/components/AuthProvider.tsx
@@ -24,7 +24,7 @@ const makeUrqlClient = () => {
 	return createClient({
 		url: saleorApiUrl,
 		suspense: true,
-		// requestPolicy: "cache-first",
+		requestPolicy: "cache-first",
 		fetch: (input, init) => saleorAuthClient.fetchWithAuth(input as NodeJS.fetch.RequestInfo, init),
 		exchanges: [dedupExchange, cacheExchange, fetchExchange],
 	});
